81ae5abf0ee4b078950c85e432bc749c81aba543
Carl Hooper <carl.hooper@malvernpanalytical.com>
2023-10-12 07:52:15 +0100

    Finished second chapter

--- PlayWrightDemo/NUnitPlayWright.cs
+++ PlayWrightDemo/NUnitPlayWright.cs
@@ -14,13 +14,19 @@ public class NUnitPlayWright : PageTest
     [Test]
     public async Task Test1()
     {
-        
+        var lnkLogin = Page.Locator("text=Login");
+
+        await lnkLogin.ClickAsync();
+
         await Page.ClickAsync("text=Login");
         await Page.FillAsync("#UserName", "admin");
         await Page.FillAsync("#Password", "password");
-        await Page.ClickAsync("text=Log in");
+
+        var btnLogin = Page.Locator("input", new PageLocatorOptions { HasTextString = "Log in" });
+        await btnLogin.ClickAsync();
+
+        //await Page.ClickAsync("text=Log in");
 
         await Expect(Page.Locator("text='Employee Details'")).ToBeVisibleAsync();
-
     }
 }
