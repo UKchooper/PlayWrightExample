1d5bbece675fa9aca0129e898fbc8c782dae794a
Carl Hooper <carl.hooper@malvernpanalytical.com>
2023-10-19 22:36:35 +0100

    Added SpecFlow - Chapter 10

---
+++ SpecFlowTestProject/StepDefinitions/EAAppTestSteps.cs
@@ -0,0 +1,31 @@
+namespace SpecFlowTestProject.StepDefinitions
+{
+    [Binding]
+    public sealed class EAAppTestSteps
+    {
+        [Given(@"I navigate to application")]
+        public void GivenINavigateToApplication()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I click login link")]
+        public void GivenIClickLoginLink()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I enter following login details")]
+        public void GivenIEnterFollowingLoginDetails(Table table)
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"I see Employee Lists")]
+        public void ThenISeeEmployeeLists()
+        {
+            throw new PendingStepException();
+        }
+
+    }
+}
